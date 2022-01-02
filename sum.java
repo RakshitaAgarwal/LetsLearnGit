86c69b27cba6b37bba6c091a9fe6019e7a8c5c4a
Rakshita <agarakshita01@gmail.com>
2022-01-02 16:51:13 +0530

    inserted sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(Syatem.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = n+b;
+        System.out.println(sum);
+    }
+}
